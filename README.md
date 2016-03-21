3c653aa58627f415f9240a66cda6e2c9940deae5
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-21 00:20:56 -0700

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# rogain-helper-defined
+
+Returns children if `data` is a non-empty Array, non-empty Object, or defined variable.  If the `<Else />` branch is defined, it will be returned when `data` is empty or undefined.
+
+```html
+<Defined data={searchResults}><Results /></Defined>
+```
+
+__Attributes__
+
+___data___
+
+Variable or Expression.
+
+## Install 
+
+With [npm](https://www.npmjs.com) do:
+
+```
+npm install rogain-helper-defined
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+const splitTrees = require('rogain-utils').splitTrees;
+module.exports = function Defined(tree, props) {
+  var split = splitTrees(tree.children, { type: 'component', name: 'Else' });
+  var res;
+
+  if (tree.data === undefined) {
+    res = split[1];
+  } else if (Array.isArray(tree.data)) {
+    res = split[tree.data.length > 0 ? 1 : 0];
+  } else if (typeof tree.data === 'object') {
+    res = split[Object.keys(tree.data).length > 0 ? 1 : 0];
+  } else {
+    res = split[0];
+  }
+
+  return res && res.length > 0 ? res : undefined;
+}
